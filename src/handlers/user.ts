c5b2e1ca145a10865f9b7222c8ca0b188d49135d
jake <jake@jakes-Laptop.local>
2024-02-21 21:16:33 -0500

    starting auth

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+import validator from 'validator';
 import { Request, Response } from 'express';
 import { db } from '../db/db';
 import { UserUpdate, NewUser, UserId } from '../models/public/User';
@@ -36,7 +38,6 @@ const deleteUser = asyncHandler(async (req: Request, res: Response) => {
 });
 
 const updateUser = asyncHandler(async (req: Request, res: Response) => {
-
 	const userId: UserId = parseInt(req.params.id) as any;
 	let userUpdate: UserUpdate = req.body
 
@@ -50,8 +51,35 @@ const updateUser = asyncHandler(async (req: Request, res: Response) => {
 	res.send(results);
 });
 
-const createUser = asyncHandler(async (req: Request, res: Response) => {
-	let user: NewUser = req.body
+const signUp = asyncHandler(async (req: Request, res: Response) => {
+	if (!validator.isEmail(req.body.email)) {
+		throw Error(`${req.body.email} is not a valid email.`)
+	}
+
+	const emailInUse = await db.selectFrom('user')
+		.select('email')
+		.where('email', '=', req.body.email)
+		.executeTakeFirst();
+
+	if (emailInUse) {
+		throw Error(`${req.body.email} is already in use.`)
+	}
+
+	const usernameInUse = await db.selectFrom('user')
+		.select('username')
+		.where('username', '=', req.body.username)
+		.executeTakeFirst();
+
+	if (usernameInUse) {
+		throw Error(`${req.body.username} is already in use.`)
+	}
+
+	let user: NewUser = {
+		username: req.body.username,
+		email: req.body.email,
+		password_hash: await bcrypt.hash(req.body.password, 10)
+	}
+
 	let results = await db.insertInto('user')
 		.values(user)
 		.returningAll()
@@ -60,4 +88,10 @@ const createUser = asyncHandler(async (req: Request, res: Response) => {
 	res.send(results);
 });
 
-export { listUsers, getUser, createUser, deleteUser, updateUser }
+const signIn = asyncHandler(async (req: Request, res: Response) => {
+});
+
+const signOut = asyncHandler(async (req: Request, res: Response) => {
+});
+
+export { listUsers, getUser, signUp, deleteUser, updateUser }
